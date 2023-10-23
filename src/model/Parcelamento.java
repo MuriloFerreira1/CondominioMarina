becd63c2307ec5c70696e123be2fad17dc32d2f4
MuriloFerreira1 <rillofelix@hotmail.com>
2023-10-23 17:59:09 -0300

    Atualização

--- src/model/Parcelamento.java
+++ src/model/Parcelamento.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Parcelamento {
-
+	private Despesa despesa;
+	private String qtdParcelas;
+	
+	
+	public Despesa getDespesa() {
+		return despesa;
+	}
+	public void setDespesa(Despesa despesa) {
+		this.despesa = despesa;
+	}
+	public String getQtdParcelas() {
+		return qtdParcelas;
+	}
+	public void setQtdParcelas(String qtdParcelas) {
+		this.qtdParcelas = qtdParcelas;
+	}
+	
+	
 }
